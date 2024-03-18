6218d418eb61247191ee9779875027b28ffb590b
Girish Gupta <67008135+girishgupta211@users.noreply.github.com>
2024-03-18 17:21:33 +0530

    added stop notebook api

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: '/home/gl_jupyter/test1/' });
+const upload = multer({ dest: '/home/gl_jupyter/submissions/' });
 const app = express();
 const { exec } = require('child_process');
 const { spawn } = require('child_process');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const port = 3015;
 
 app.get('/', (req, res) => {
@@ -14,13 +16,6 @@ app.get('/', (req, res) => {
 app.get('/health', (req, res) => {
     res.json({ status: 'OK' });
 });
-
-// app.get('/notebook', (req, res) => {
-//     res.send(`
-//       <iframe src="http://localhost:8888/notebooks/notebook.ipynb" width="100%" height="500px"></iframe>
-//     `);
-//   });
-
 
 app.get('/notebook', (req, res) => {
     // gljpc/notebooks/notebook.ipynb
@@ -47,19 +42,27 @@ app.post('/run-notebook', upload.any(), (req, res) => {
     console.log('Dataset File path:', datasetFile.path);
 
     console.log('Running the Jupyter notebook server...');
-
-    exec('adduser --disabled-password --gecos "" gl_jupyter', (error, stdout, stderr) => {
+    exec('id -u gl_jupyter', (error, stdout, stderr) => {
         if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+            // User does not exist, create it
+            exec('adduser --disabled-password --gecos "" gl_jupyter', (error, stdout, stderr) => {
+                if (error) {
+                    console.log(`error: ${error.message}`);
+                    return;
+                }
+                if (stderr) {
+                    console.log(`stderr: ${stderr}`);
+                    return;
+                }
+                console.log(`stdout: ${stdout}`);
+            });
+        } else {
+            // User exists, log a message
+            console.log('User gl_jupyter already exists');
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
 
-        exec('cd /home/gl_jupyter/ && mkdir -p test1 && chown -R gl_jupyter:gl_jupyter test1', (error, stdout, stderr) => {
+        // Proceed with creating directory and changing ownership
+        exec('cd /home/gl_jupyter/ && mkdir -p submissions && chown -R gl_jupyter:gl_jupyter submissions', (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
                 return;
@@ -72,42 +75,31 @@ app.post('/run-notebook', upload.any(), (req, res) => {
         });
     });
 
+    function copyFile(sourcePath, destinationPath, fileName, res) {
+        console.log(`Copying the ${fileName} to ${destinationPath} directory...`);
+        exec(`cp ${sourcePath} ${destinationPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Failed to copy ${fileName}: ${error.message}`);
+                res.status(500).json({ status: 'Error', message: error.message });
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+    }
 
-    console.log('Copying the dataset file to /home/gl_jupyter/test1 directory...');
-    exec(`cp ${datasetFile.path} /home/gl_jupyter/test1/dataset.csv`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Failed to copy dataset file: ${error.message}`);
-            res.status(500).json({ status: 'Error', message: error.message });
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    copyFile(datasetFile.path, '/home/gl_jupyter/submissions/dataset.csv', 'dataset file', res);
+    copyFile(notebookFile.path, '/home/gl_jupyter/submissions/notebook.ipynb', 'notebook file', res);
 
-    // copy the notebook file to /home/gl_jupyter/test1 directory as notebook.ipynb
-    console.log('Copying the notebook file to /home/gl_jupyter/test1 directory...');
-    exec(`cp ${notebookFile.path} /home/gl_jupyter/test1/notebook.ipynb`, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Failed to copy notebook file: ${error.message}`);
-            res.status(500).json({ status: 'Error', message: error.message });
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-    });
+    // const notebookServer = spawn('/usr/bin/sudo -u gl_jupyter env -i jupyter', ['notebook', '--ip=*', '--allow-root', '--port=8888', `--NotebookApp.notebook_dir='/home/gl_jupyter/submissions'`, `--NotebookApp.default_url=/notebooks/notebook.ipynb`]);
+    const notebookServer = spawn('jupyter', ['notebook', '--ip=*', '--allow-root', '--port=8888', `--NotebookApp.notebook_dir='/home/gl_jupyter/submissions'`, `--NotebookApp.default_url=/notebooks/notebook.ipynb`]);
 
-
-    const notebookServer = spawn('jupyter', ['notebook', '--ip=*', '--allow-root', '--port=8888', `--NotebookApp.notebook_dir='/home/gl_jupyter/test1'`, `--NotebookApp.default_url=/notebooks/notebook.ipynb`]);
-
-    notebookServerPid = notebookServer.pid;
+    const notebookServerPid = notebookServer.pid;
     console.log(`Jupyter notebook server PID: ${notebookServerPid}`);
-    
+
     notebookServer.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
         const output = data.toString();
@@ -118,6 +110,7 @@ app.post('/run-notebook', upload.any(), (req, res) => {
 
     notebookServer.stderr.on('data', (data) => {
         console.error(`stderr notebook: ${data}`);
+        // res.status(500).json({ status: 'Error', message: `stderr notebook: ${data}` });
     });
 
     notebookServer.on('error', (error) => {
@@ -129,9 +122,13 @@ app.post('/run-notebook', upload.any(), (req, res) => {
         console.log(`Jupyter notebook server exited with code ${code}`);
     });
 
+    res.json({ status: 'OK' });
 });
 
 app.post('/stop-notebook', (req, res) => {
+    console.log('Request body:', req.body);
+    console.log('Request body:', JSON.stringify(req.body));
+    
     const port = req.body.port;
     console.log(`Stopping Jupyter notebook server on port ${port}...`);
 
@@ -156,15 +153,6 @@ app.post('/stop-notebook', (req, res) => {
     });
 });
 
-
-app.post('/stop-notebook', (req, res) => {
-    if (notebookServer) {
-        notebookServer.kill('SIGTERM');
-        res.json({ status: 'OK', message: 'Jupyter notebook server stopped' });
-    } else {
-        res.status(500).json({ status: 'Error', message: 'Jupyter notebook server is not running' });
-    }
-});
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
